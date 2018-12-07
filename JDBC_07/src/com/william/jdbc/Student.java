5019d1d54f1b2ee699604141d1280819a3c03843
wangqinggang <599252594@qq.com>
2018-12-07 15:12:50 +0800

    任务7： JDBC_PreparedStatement
    任务7： JDBC_PreparedStatement

---
+++ JDBC_07/src/com/william/jdbc/Student.java
@@ -0,0 +1,97 @@
+package com.william.jdbc;
+
+public class Student {
+	// 流水号
+	private int flowId;
+	// 考试类型
+	private int type;
+	// 身份证号
+	private String idCard;
+	// 准考证号
+	private String examCard;
+	// 学生名
+	private String studentName;
+	// 学生地址
+	private String Location;
+	// 考试分数
+	private int grade;
+
+	public int getFlowId() {
+		return flowId;
+	}
+
+	public void setFlowId(int flowId) {
+		this.flowId = flowId;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getIdCard() {
+		return idCard;
+	}
+
+	public void setIdCard(String idCard) {
+		this.idCard = idCard;
+	}
+
+	public String getExamCard() {
+		return examCard;
+	}
+
+	public void setExamCard(String examCard) {
+		this.examCard = examCard;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getLocation() {
+		return Location;
+	}
+
+	public void setLocation(String location) {
+		Location = location;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public Student(int flowId, int type, String idCard, String examCard, String studentName, String location,
+			int grade) {
+		super();
+		this.flowId = flowId;
+		this.type = type;
+		this.idCard = idCard;
+		this.examCard = examCard;
+		this.studentName = studentName;
+		Location = location;
+		this.grade = grade;
+	}
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Student [flowId=" + flowId + ", type=" + type + "," + " idCard=" + idCard + ", examCard=" + examCard
+				+ ", studentName=" + studentName + ", Location=" + Location + ", grade=" + grade + "]";
+	}
+
+}
